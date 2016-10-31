21abf671e36d69d5c1a55c0b77369d2eeed82536
Val Chapman <vchapman@iastate.edu>
2016-10-31 14:09:45 -0500

    More Developed Generation

--- Weight-Lifting/app/src/main/java/se329/com/weight_lifting/buildWorkout.java
+++ Weight-Lifting/app/src/main/java/se329/com/weight_lifting/buildWorkout.java
@@ -2,6 +2,8 @@ package se329.com.weight_lifting;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class buildWorkout extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class buildWorkout extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_build_workout);
+        Spinner dropdown = (Spinner)findViewById(R.id.spinner1);
+        String[] items = new String[]{"Free Weights", "Machines", "Natural"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
+        dropdown = (Spinner) findViewById(R.id.spinner2);
+        String[] mGroups = new String[]{"Chest and Triceps", "Biceps and Back", "Legs", "Random"};
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, mGroups);
+        dropdown.setAdapter(adapter2);
+
     }
 }

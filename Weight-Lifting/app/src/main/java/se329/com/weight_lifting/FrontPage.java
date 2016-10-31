aafc376785696697795bf94b84ade9201648c96f
Val Chapman <vchapman@iastate.edu>
2016-10-31 12:47:16 -0500

    Non Graphical Front page

---
+++ Weight-Lifting/app/src/main/java/se329/com/weight_lifting/FrontPage.java
@@ -0,0 +1,21 @@
+package se329.com.weight_lifting;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class FrontPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_front_page);
+    }
+
+    public void buildWorkout(View view){
+        Intent intent = new Intent(this, buildWorkoutActivity.class);
+        startActivity(intent);
+
+    }
+}
